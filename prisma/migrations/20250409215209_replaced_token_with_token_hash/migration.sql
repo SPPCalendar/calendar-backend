ff011f2c403987948fe7ae2e4aa8fb94c6df6dc9
Volodymyr Beimuk <tetrenkuchioan@gmail.com>
2025-04-10 01:38:54 +0300

    add refresh token hashing

---
+++ prisma/migrations/20250409215209_replaced_token_with_token_hash/migration.sql
@@ -0,0 +1,17 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `token` on the `RefreshToken` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[token_hash]` on the table `RefreshToken` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `token_hash` to the `RefreshToken` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropIndex
+DROP INDEX "RefreshToken_token_key";
+
+-- AlterTable
+ALTER TABLE "RefreshToken" DROP COLUMN "token",
+ADD COLUMN     "token_hash" TEXT NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "RefreshToken_token_hash_key" ON "RefreshToken"("token_hash");
--- src/services/tokenService.ts
+++ src/services/tokenService.ts
@@ -1,25 +1,36 @@
+import crypto from 'crypto'
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
+
+export const hashToken = (token: string): string =>
+  crypto.createHash('sha256').update(token).digest('hex')
+
 export const saveRefreshToken = async (userId: number, token: string, expiresAt: Date) => {
+  const token_hash = hashToken(token)
+  
   return prisma.refreshToken.create({
     data: {
       userId,
-      token,
+      token_hash,
       expiresAt,
     },
   })
 }
 
 export const deleteRefreshToken = async (token: string) => {
+  const token_hash = hashToken(token)
+
   return prisma.refreshToken.deleteMany({
-    where: { token },
+    where: { token_hash },
   })
 }
 
 export const isRefreshTokenValid = async (token: string): Promise<boolean> => {
+  const token_hash = hashToken(token)
+
   const found = await prisma.refreshToken.findUnique({
-    where: { token },
+    where: { token_hash },
   })
   return !!found
 }
